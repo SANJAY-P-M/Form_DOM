e5577acb823517e2c3a2679d07c083c0b32965d3
SANJAY-P-M <pmsanjay2003@gmail.com>
2023-10-14 18:13:04 +0530

    Completed the task

--- submit.js
+++ submit.js
@@ -1,16 +1,25 @@
 function displayValue(){
     let name = document.getElementById('name').value;
     let department = document.getElementById('department').value;
-    let sportsCollection = document.getElementsByName('sports');
-    // let sports;
-    // for(i =0;i< sportsCollection.length;i++){
-    //     if (sportsCollection[i].checked == true) {
-    //         sports = sportsCollection.values;
-    //         break;
-    //     }
-    // }
-    alert("NAME : " + name + "  DEPARTMENT : " + department )
-    // + " sports : " + sports);
-    console.log(document.getElementsByName('sports'))
-    //   + "sports : " + getSports() +"language known : " + getLanguage());
+
+    // analyzing selected radio button
+    let sports;
+    document.getElementsByName('sports').forEach((element)=>{
+        if (element.checked == true) {
+            sports = element.id;
+        }
+    });
+
+
+    // analyzing selected checkbox
+    let language = [];
+    document.getElementsByName('language').forEach((element)=>{
+        if (element.checked == true) {
+            language.push( element.id);
+        }
+    });
+    
+
+    // final alert message 
+    alert("NAME : " + name + "  DEPARTMENT : " + department +" SPORTS : " +sports + " LANGUAGES KNOWN : " + language)
 }
